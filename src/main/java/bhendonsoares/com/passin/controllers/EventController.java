09d2f012e87092f73341436771bd9967d32bd9ae
Bhendon Soares <bhendon.soares12@gmail.com>
2024-04-05 12:45:46 -0300

    feat: adiciona endpoint de criação de participante e exibir crachá

--- src/main/java/bhendonsoares/com/passin/controllers/EventController.java
+++ src/main/java/bhendonsoares/com/passin/controllers/EventController.java
@@ -1,5 +1,7 @@
 package bhendonsoares.com.passin.controllers;
 
+import bhendonsoares.com.passin.dto.attendee.AttendeeIdDTO;
+import bhendonsoares.com.passin.dto.attendee.AttendeeRequestDTO;
 import bhendonsoares.com.passin.dto.attendee.AttendeesListResponseDTO;
 import bhendonsoares.com.passin.dto.event.EventIdDTO;
 import bhendonsoares.com.passin.dto.event.EventRequestDTO;
@@ -30,6 +32,15 @@ public class EventController {
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeIdDTO attendeeIdDTO = this.eventService.registerAttendeeOnEvent(eventId, body);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
     @GetMapping("/attendees/{id}")

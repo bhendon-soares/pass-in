ada1d5cc03aa3f062974f90b280372a9e7b096cf
Bhendon Soares <bhendon.soares12@gmail.com>
2024-04-05 13:38:30 -0300

    feat: adiciona tratamento de exceções

--- src/main/java/bhendonsoares/com/passin/config/ExceptionEntityHandler.java
+++ src/main/java/bhendonsoares/com/passin/config/ExceptionEntityHandler.java
@@ -1,6 +1,12 @@
 package bhendonsoares.com.passin.config;
 
+import bhendonsoares.com.passin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import bhendonsoares.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import bhendonsoares.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import bhendonsoares.com.passin.domain.event.exceptions.EventFullException;
 import bhendonsoares.com.passin.domain.event.exceptions.EventNotFoundException;
+import bhendonsoares.com.passin.dto.general.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,5 +16,25 @@ public class ExceptionEntityHandler {
     @ExceptionHandler(EventNotFoundException.class)
     public ResponseEntity handleEventNotFound(EventNotFoundException exception){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception){
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistsException.class)
+    public ResponseEntity handleAttendeeAlreadyExists(AttendeeAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExists(CheckInAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 }
---
+++ src/main/java/bhendonsoares/com/passin/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package bhendonsoares.com.passin.dto.general;
+
+public record ErrorResponseDTO(String message) {
+}
